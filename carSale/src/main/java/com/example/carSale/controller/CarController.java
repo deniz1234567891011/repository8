f6319ba2fc8c98085f8d70bf32effc5173e651b7
deniz12345678910 <76062600+deniz12345678910@users.noreply.github.com>
2025-02-05 19:49:12 +0400

    Responsible Added
    I Added The Responsible

--- carSale/src/main/java/com/example/carSale/controller/CarController.java
+++ carSale/src/main/java/com/example/carSale/controller/CarController.java
@@ -4,12 +4,14 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.carSale.Main.Car;
@@ -21,6 +23,7 @@ public class CarController {
 	@Autowired
 CarService cService;
 @GetMapping("get-all")
+@ResponseStatus(HttpStatus.ACCEPTED)
 public List<Car> findAll(){
 	return cService.findAllCars();
 }
@@ -29,10 +32,11 @@ public List<Car> findAll(){
 public Optional<Car> findById(Integer id){
 	return cService.findById(id);
 }
-
+@ResponseStatus(HttpStatus.CREATED)
 @PostMapping("add")
 public void save(@RequestBody Car car) {
 	cService.saveCar(car);
+
 }
 
 }
